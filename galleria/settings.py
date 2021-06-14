1bfd72992bdbe8ae5a81f8110a09c862c05b8635
jaketrent <trent.jake@gmail.com>
2021-06-14 05:48:06 -0600

    get secret from env; turn off debug in prod

--- galleria/settings.py
+++ galleria/settings.py
@@ -25,11 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gd%(cz-8eg0+t_5sjr7h7pmf-_r-c8#b4%0id!!&pyd2(1g@f4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('ENV') != 'production'
 
 ALLOWED_HOSTS = []
 
